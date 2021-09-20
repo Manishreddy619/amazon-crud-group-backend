97aa03e604cd71e2a087584a5e75d894fe41dcaf
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-09-20 11:04:51 +0200

    error handlers done
    done...............

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 
 import { join } from 'path';
-
+import {
+	BadRequest,
+	unAuthorized,
+	notfound,
+	forbiden,
+	serverside,
+} from './errorHandlers.js';
 import productsRouter from './resources/products/products.js';
 import ReviewsRouter from './resources/reviews/reviews.js';
 import cors from 'cors';
+import { notfound } from './errorHandlers.js';
 
 const server = express(); ///
 
@@ -20,6 +27,12 @@ server.use(express.json());
 ////// listening to server
 server.use('/products', productsRouter);
 server.use('/reviews', ReviewsRouter);
+
+server.use(unAuthorized);
+server.use(BadRequest);
+server.use(notfound);
+server.use(forbiden);
+server.use(serverside);
 server.listen(PORT, () => {
 	console.log(`we are listening on ${PORT}`);
 });
