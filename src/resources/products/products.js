fbd1fde731de2dd12a64df2dd79e62f238e5045e
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-09-20 15:26:51 +0200

    added validator

--- src/resources/products/products.js
+++ src/resources/products/products.js
@@ -1,58 +1,71 @@
-import express from 'express';
-import multer from 'multer';
+import express from "express";
+import multer from "multer";
 import uniqid from "uniqid";
-import createHttpError from 'http-errors';
+import createHttpError from "http-errors";
+import { productsValidator } from "./productsValidation.js";
+import { validationResult } from "express-validator";
 
-import { readProducts, writeProducts, saveImages } from '../../utilis.js';
+import { readProducts, writeProducts, saveImages } from "../../utilis.js";
 const productsRouter = express.Router();
 
+productsRouter.post("/", multer().single("productImg"), productsValidator, async (req, res, next) => {
+    const errorsList = validationResult(req);
+    if (!errorsList.isEmpty()) {
+      next(createHttpError(400, { message: errorsList }));
 
-productsRouter.post('/', multer().single('productImg'), async (req, res, next) => {
-    try {
-        await saveImages(req.file.originalname, req.file.buffer)
-        // console.log(req.file)
-        // res.send('ok')
+    } else {
+      try {
+        await saveImages(req.file.originalname, req.file.buffer);
         const newProduct = {
-            ...req.body,
-            imageUrl: `http://localhost:3002/products/images/${req.file.originalname}`,
-            id: uniqid(),
-            createdAt: new Date()
-        }
+          ...req.body,
+          imageUrl: `http://localhost:3002/products/images/${req.file.originalname}`,
+          id: uniqid(),
+          createdAt: new Date(),
+        };
         // console.log(newProduct)
-        const productsJSON = await readProducts()
-        productsJSON.push(newProduct)
-        await writeProducts(productsJSON)
-        console.log("PRODUCTS JSON", productsJSON)
-        res.status(201).send(newProduct)
-    } catch (error) {
-       next(createHttpError(403), 'Some required fields are missing!')
-    }
-})
+        const productsJSON = await readProducts();
+        productsJSON.push(newProduct);
+        await writeProducts(productsJSON);
+        console.log("PRODUCTS JSON", productsJSON);
+        res.status(201).send(newProduct);
 
-productsRouter.put('/:productId', multer().single('productImg'), async (req, res, next) => {
-    try {
-        const productsJSON = await readProducts()
-        const productIndex = productsJSON.findIndex(product => product.id === req.params.productId)
-        if(productIndex !== -1){
-            const previousData = productsJSON[productIndex]
-            const updatedData = {
-                ...previousData, 
-                ...req.body, 
-                imageUrl: `http://localhost:3002/products/images/${req.file.originalname}`,
-                updatedAt: new Date(),
-                id: req.params.productId
-            }
-            console.log(updatedData)
-            productsJSON[productIndex] = updatedData
-            // productsJSON.push(updatedData)
-            await writeProducts(productsJSON)
-            res.send(updatedData)
-        } else {
-            next(createHttpError(404), `No product found with id ${product.id}`)
-        }
-    } catch (error) {
-        next(error)
+      } catch (error) {
+        next(createHttpError(403), "Some required fields are missing!");
+      }
     }
-})
+  }
+);
+
+productsRouter.put("/:productId", multer().single("productImg"), productsValidator, async (req, res, next) => {
+    const errorsList = validationResult(req);
+    if (!errorsList.isEmpty()){
+        next(createHttpError(400, { message: errorsList }));
+    } else {
+        try {
+          const productsJSON = await readProducts();
+          const productIndex = productsJSON.findIndex(
+            (product) => product.id === req.params.productId
+          );
+          if (productIndex !== -1) {
+            const previousData = productsJSON[productIndex];
+            const updatedData = {
+              ...previousData,
+              ...req.body,
+              imageUrl: `http://localhost:3002/products/images/${req.file.originalname}`,
+              updatedAt: new Date(),
+              id: req.params.productId,
+            };
+            productsJSON[productIndex] = updatedData;
+            await writeProducts(productsJSON);
+            res.send(updatedData);
+          } else {
+            next(createHttpError(404), `No product found with id ${product.id}`);
+          }
+        } catch (error) {
+          next(error);
+        }
+    }
+  }
+);
 
 export default productsRouter;
